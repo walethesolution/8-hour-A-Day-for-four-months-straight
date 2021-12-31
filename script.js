c608a1f200fff56a0065b0039bbb185bbcb9528d
walethesolution <93170147+walethesolution@users.noreply.github.com>
2021-12-31 10:04:46 -0500

    initial commit for day 3 added a script.js file

---
+++ script.js
@@ -0,0 +1,3 @@
+let today = new Date();
+let date = `${today.getMonth() + 1}-${today.getDate()}-${today.getFullYear()}`;
+let time = `${today.getHours()} :  ${today.getMinutes()} : ${today.getSeconds()}`;
